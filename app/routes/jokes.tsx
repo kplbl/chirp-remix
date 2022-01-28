dd794641946f44eec894bd59612ab9ac89c59a36
kplbl <51096422+kplbl@users.noreply.github.com>
2022-01-28 12:03:12 +0100

    logout

--- app/routes/jokes.tsx
+++ app/routes/jokes.tsx
@@ -8,19 +8,29 @@ import {
 import stylesUrl from "../styles/jokes.css";
 import type { Joke } from "@prisma/client";
 import { db } from "~/utils/db.server";
+import { getUser } from "~/utils/session.server";
+import { User } from "@prisma/client";
 
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: stylesUrl }];
 };
 
-type LoaderData = { jokeListItems: Array<{ id: string; name: string }> };
-export const loader: LoaderFunction = async () => {
+type LoaderData = {
+  user: User | null;
+  jokeListItems: Array<{ id: string; name: string }>;
+};
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const jokeListItems = await db.joke.findMany({
+    take: 5,
+    orderBy: { createdAt: "desc" },
+    select: { id: true, name: true },
+  });
+  const user = await getUser(request);
+
   const data: LoaderData = {
-    jokeListItems: await db.joke.findMany({
-      take: 5,
-      select: { id: true, name: true },
-      orderBy: { createdAt: "desc" },
-    }),
+    jokeListItems,
+    user,
   };
   return data;
 };
@@ -37,6 +47,18 @@ export default function JokesRoute() {
               <span className="logo-medium">J🤪KES</span>
             </Link>
           </h1>
+          {data.user ? (
+            <div className="user-info">
+              <span>{`Hi ${data.user.username}`}</span>
+              <form action="/logout" method="post">
+                <button type="submit" className="button">
+                  Logout
+                </button>
+              </form>
+            </div>
+          ) : (
+            <Link to="/login">Login</Link>
+          )}
         </div>
       </header>
       <main className="jokes-main">
