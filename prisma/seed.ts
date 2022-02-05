e35b0cdb36e13c6d5c3b11c8d93b81072f54cb73
kplbl <51096422+kplbl@users.noreply.github.com>
2022-02-05 18:15:14 +0100

    schema change

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -13,7 +13,7 @@ async function seed() {
 
   await Promise.all(
     getPosts().map((post) => {
-      const data = { posterId: kody.id, ...post };
+      const data = { posterId: kody.id, tags: "", ...post };
       return db.post.create({ data });
     })
   );
@@ -26,31 +26,31 @@ function getPosts() {
 
   return [
     {
-      name: "Road worker",
+      title: "Road worker",
       content: `I never wanted to believe that my Dad was stealing from his job as a road worker. But when I got home, all the signs were there.`,
     },
     {
-      name: "Frisbee",
+      title: "Frisbee",
       content: `I was wondering why the frisbee was getting bigger, then it hit me.`,
     },
     {
-      name: "Trees",
+      title: "Trees",
       content: `Why do trees seem suspicious on sunny days? Dunno, they're just a bit shady.`,
     },
     {
-      name: "Skeletons",
+      title: "Skeletons",
       content: `Why don't skeletons ride roller coasters? They don't have the stomach for it.`,
     },
     {
-      name: "Hippos",
+      title: "Hippos",
       content: `Why don't you find hippopotamuses hiding in trees? They're really good at it.`,
     },
     {
-      name: "Dinner",
+      title: "Dinner",
       content: `What did one plate say to the other plate? Dinner is on me!`,
     },
     {
-      name: "Elevator",
+      title: "Elevator",
       content: `My first time using an elevator was an uplifting experience. The second time let me down.`,
     },
   ];
