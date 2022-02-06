9a4c88618292a37462fe0fb082626df32c59ca5f
kplbl <51096422+kplbl@users.noreply.github.com>
2022-02-06 21:31:46 +0100

    schema change

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,7 +5,6 @@ async function seed() {
   const kody = await db.user.create({
     data: {
       username: "kody",
-      userFullname: "kodiak bear",
       // this is a hashed version of "twixrox"
       passwordHash:
         "$2b$10$K7L1OJ45/4Y2nIvhRVpCe.FSmhDdWoXehVzJptJ/op0lSsvqNu/1u",
