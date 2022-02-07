69df31d9f503481edebcd2524589e7db7d1d9d14
kplbl <51096422+kplbl@users.noreply.github.com>
2022-02-07 09:35:08 +0100

    moved

--- app/routes/posts.tsx
+++ app/routes/posts.tsx
@@ -27,50 +27,8 @@ export const loader: LoaderFunction = async ({ request }) => {
 export default function PostsRoute() {
   const data = useLoaderData<LoaderData>();
   return (
-    <div className="jokes-layout">
-      <header className="jokes-header">
-        <div className="container">
-          <h1 className="home-link">
-            <Link to="/" title="Blog Posts" aria-label="Blog Posts">
-              <span className="logo">🤪</span>
-              <span className="logo-medium">Posts</span>
-            </Link>
-          </h1>
-          {data.user ? (
-            <div className="user-info">
-              <span>{`Hi ${data.user.username}`}</span>
-              <form action="/logout" method="post">
-                <button type="submit" className="button">
-                  Logout
-                </button>
-              </form>
-            </div>
-          ) : (
-            <Link to="/login">Login</Link>
-          )}
-        </div>
-      </header>
-      <main className="jokes-main">
-        <div className="container">
-          <div className="jokes-list">
-            <Link to=".">Get a random post</Link>
-            <p>Here are a few more posts to check out:</p>
-            <ul>
-              {data.postListItems.map((post) => (
-                <li key={post.id}>
-                  <Link to={post.id}>{post.title}</Link>
-                </li>
-              ))}
-            </ul>
-            <Link to="new" className="button">
-              Add your own
-            </Link>
-          </div>
-          <div className="jokes-outlet">
-            <Outlet />
-          </div>
-        </div>
-      </main>
+    <div className="">
+      
     </div>
   );
 }
