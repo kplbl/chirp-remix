ba584ffceccc84a39df381ee26d94d88272343fc
kplbl <51096422+kplbl@users.noreply.github.com>
2022-02-07 15:54:53 +0100

    icons

--- app/root.tsx
+++ app/root.tsx
@@ -12,6 +12,12 @@ import { getUser } from "./utils/session.server";
 import { User } from "@prisma/client";
 import type { LinksFunction } from "remix";
 import styles from "./styles/app.css";
+import {
+  HomeIcon,
+  PencilIcon,
+  LogoutIcon,
+  LoginIcon,
+} from "@heroicons/react/outline";
 
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: styles }];
@@ -53,40 +59,14 @@ function Document({
             <div className="">
               <Link to="/" className="flex gap-3">
                 <div className="p-1">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-9 w-9"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-                    />
-                  </svg>
+                  <HomeIcon className="h-9 w-9" />
                 </div>
 
                 <div className="text-xl hidden md:flex items-center">Home</div>
               </Link>
               <Link to="/new" className="flex gap-3">
                 <div className="bg-blue-400 rounded-full p-1">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-9 w-9 text-blue-100"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
-                    />
-                  </svg>
+                  <PencilIcon className="h-9 w-9 text-blue-100" />
                 </div>
 
                 <div className="text-xl hidden md:flex items-center">Chirp</div>
@@ -97,20 +77,7 @@ function Document({
                 {data.user ? (
                   <form action="/logout" method="post" className="flex gap-2">
                     <button type="submit" className="button">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-10 w-10"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth={2}
-                          d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
-                        />
-                      </svg>
+                      <LogoutIcon className="w-10 h-10" />
                     </button>
                     <div className="text-xl hidden md:flex items-center">
                       Logout
@@ -118,20 +85,7 @@ function Document({
                   </form>
                 ) : (
                   <Link to="/login" className="flex gap-3">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-10 w-10"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"
-                      />
-                    </svg>
+                    <LoginIcon className="w-10 h-10" />
                     <div className="text-xl hidden md:flex items-center">
                       Login
                     </div>
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,6 +2,8 @@ import { Link, LoaderFunction, useLoaderData, Outlet } from "remix";
 import { db } from "~/utils/db.server";
 import { getUser } from "~/utils/session.server";
 import { User } from "@prisma/client";
+import * as timeago from "timeago.js";
+import { ChatIcon, HeartIcon, ShareIcon } from "@heroicons/react/outline";
 
 type LoaderData = {
   user: User | null;
@@ -10,15 +12,23 @@ type LoaderData = {
     title: string;
     content: string;
     poster: User;
+    createdAt: Date;
   }>;
 };
 
 export const loader: LoaderFunction = async ({ request }) => {
   const postListItems = await db.post.findMany({
-    take: 5,
+    take: 15,
     orderBy: { createdAt: "desc" },
-    select: { id: true, title: true, content: true, poster: true },
+    select: {
+      id: true,
+      title: true,
+      content: true,
+      poster: true,
+      createdAt: true,
+    },
   });
+
   const user = await getUser(request);
 
   const data: LoaderData = {
@@ -35,56 +45,29 @@ export default function IndexRoute() {
       <main className="max-w-2xl">
         {data.postListItems.map((post) => (
           <div
-            className="p-5 border border-gray-200 last-of-type:border-b border-b-0"
+            className="p-5 border border-gray-200 last-of-type:border-b border-b-0 flex gap-5"
             key={post.id}
           >
-            <Link to={`/posts/${post.id}`}>
-              {post.title} @{post.poster.username}
-            </Link>
-            <div>{post.content}</div>
-            <div className="flex justify-evenly mt-5">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
-                />
-              </svg>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                />
-              </svg>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12"
-                />
-              </svg>
+            <div
+              className=" h-16 w-16 bg-slate-200 rounded-full overflow-clip"
+              dangerouslySetInnerHTML={{
+                __html: post.poster.avatarSVG || "",
+              }}
+            ></div>
+
+            <div className="flex flex-col flex-1">
+              <Link to={`/posts/${post.id}`}>
+                {post.title}{" "}
+                <span className="text-slate-600  text-sm">
+                  @{post.poster.username} {timeago.format(post.createdAt)}
+                </span>
+              </Link>
+              <div>{post.content}</div>
+              <div className="flex justify-between mt-5 px-8">
+                <ChatIcon className="h-6 w-6" />
+                <HeartIcon className="h-6 w-6" />
+                <ShareIcon className="h-6" />
+              </div>
             </div>
           </div>
         ))}
